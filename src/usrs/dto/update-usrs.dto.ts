01f545c7e3c1a8f209d71076f1ce6cec36c9aaf2
elio-info <dario.andreu@gmail.com>
2024-03-27 15:05:42 -0400

    poner dot

--- src/usrs/dto/update-usrs.dto.ts
+++ src/usrs/dto/update-usrs.dto.ts
@@ -1,7 +1,7 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { CreateUsrsDto } from "./create-usrs.dto";
 
-export class UpdateUsrsDto extends PartialType
+export class UpdateUsrsDto extends PartialType(CreateUsrsDto)
     {         
         cargo:'Inv'|'Obr'|'Jef';
         autor:string;
